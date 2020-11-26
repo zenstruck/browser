e1f5ac00f87476e51ec1bb33d9d49228cac93df6
Kevin Bond <kevinbond@gmail.com>
2020-11-26 10:48:50 -0500

    add Browser test trait

---
+++ src/Browser/Test/Browser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zenstruck\Browser\Test;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser as DefaultBrowser;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ *
+ * @mixin KernelTestCase
+ */
+trait Browser
+{
+    final public function browser(): DefaultBrowser
+    {
+        if (!$this instanceof KernelTestCase) {
+            throw new \RuntimeException(\sprintf('The "%s" trait can only be used on TestCases that extend "%s".', __TRAIT__, KernelTestCase::class));
+        }
+
+        // reboot kernel before starting browser
+        static::bootKernel();
+
+        return $this->createBrowser();
+    }
+
+    protected function createBrowser(): DefaultBrowser
+    {
+        return new DefaultBrowser(static::$container->get('test.client'));
+    }
+}
