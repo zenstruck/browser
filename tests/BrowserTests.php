5352f6fbb61133a8be17df8c44981e480f49fbb0
Kevin Bond <kevinbond@gmail.com>
2020-12-06 07:02:36 -0500

    fix multi-select bug

--- tests/BrowserTests.php
+++ tests/BrowserTests.php
@@ -285,6 +285,7 @@ trait BrowserTests
             ->uncheckField('input3')
             ->selectFieldOption('input4', 'option 2')
             ->attachFile('input5', __FILE__)
+            ->selectFieldOptions('input6', ['option 1', 'option 3'])
             ->press('Submit')
             ->assertOn('/submit-form')
             ->assertResponseContains('"input_1":"Kevin"')
@@ -292,19 +293,6 @@ trait BrowserTests
             ->assertResponseNotContains('"input_3')
             ->assertResponseContains('"input_4":"option 2"')
             ->assertResponseContains(\sprintf('"input_5":"%s"', \pathinfo(__FILE__, PATHINFO_BASENAME)))
-        ;
-    }
-
-    /**
-     * @test
-     */
-    public function form_multiselect(): void
-    {
-        $this->browser()
-            ->visit('/page1')
-            ->selectFieldOptions('input6', ['option 1', 'option 3'])
-            ->press('Submit')
-            ->assertOn('/submit-form')
             ->assertResponseContains('"input_6":["option 1","option 3"]')
         ;
     }
