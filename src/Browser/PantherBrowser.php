12508ddca162abc6da27ec7c8717264b4e6c1647
chris <chris@sky-scripts.de>
2022-08-26 15:24:05 +0200

    Update src/Browser/PantherBrowser.php
    Co-authored-by: Kevin Bond <kevinbond@gmail.com>

--- src/Browser/PantherBrowser.php
+++ src/Browser/PantherBrowser.php
@@ -211,7 +211,7 @@ class PantherBrowser extends Browser
         return $this;
     }
 
-    public function rightClick(string $selector): self
+    final public function rightClick(string $selector): self
     {
         $element = $this->getClickableElement($selector);
         $element->rightClick();
