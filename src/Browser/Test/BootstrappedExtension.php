eb1e294b73ef59f6031dcb84bc156df86c6ee269
marek <m.necesany@choosit.com>
2023-11-30 11:51:20 +0100

    phpunit 10 bootstrap proposal

---
+++ src/Browser/Test/BootstrappedExtension.php
@@ -0,0 +1,135 @@
+<?php
+
+/*
+ * This file is part of the zenstruck/browser package.
+ *
+ * (c) Kevin Bond <kevinbond@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zenstruck\Browser\Test;
+
+use PHPUnit\Event\Code\Test;
+use PHPUnit\Event\Code\TestMethod;
+use PHPUnit\Event\Test\Errored;
+use PHPUnit\Event\Test\ErroredSubscriber;
+use PHPUnit\Event\Test\Failed;
+use PHPUnit\Event\Test\FailedSubscriber;
+use PHPUnit\Event\Test\Finished as TestFinishedEvent;
+use PHPUnit\Event\Test\FinishedSubscriber as TestFinishedSubscriber;
+use PHPUnit\Event\Test\PreparationStarted as TestStartedEvent;
+use PHPUnit\Event\Test\PreparationStartedSubscriber as TestStartedSubscriber;
+use PHPUnit\Event\TestRunner\Finished as TestRunnerFinishedEvent;
+use PHPUnit\Event\TestRunner\FinishedSubscriber as TestRunnerFinishedSubscriber;
+use PHPUnit\Event\TestRunner\Started as TestRunnerStartedEvent;
+use PHPUnit\Event\TestRunner\StartedSubscriber as TestRunnerStartedSubscriber;
+use PHPUnit\Runner\Extension\Extension;
+use PHPUnit\Runner\Extension\Facade;
+use PHPUnit\Runner\Extension\ParameterCollection;
+use PHPUnit\TextUI\Configuration\Configuration;
+use Zenstruck\Browser;
+
+class BootstrappedExtension implements Extension
+{
+    public function bootstrap(Configuration $configuration, Facade $facade, ParameterCollection $parameters): void
+    {
+        $extension = new LegacyExtension();
+
+        $facade->registerSubscriber(new class($extension) implements TestRunnerStartedSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+
+            public function notify(TestRunnerStartedEvent $event): void
+            {
+                $this->extension->executeBeforeFirstTest();
+            }
+        });
+
+        $facade->registerSubscriber(new class($extension) implements TestRunnerFinishedSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+
+            public function notify(TestRunnerFinishedEvent $event): void
+            {
+                $this->extension->executeAfterLastTest();
+            }
+        });
+
+        $facade->registerSubscriber(new class($extension) implements TestStartedSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+            public function notify(TestStartedEvent $event): void
+            {
+                $this->extension->executeBeforeTest($event->test()->name());
+            }
+        });
+
+        $facade->registerSubscriber(new class($extension) implements TestFinishedSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+
+            public function notify(TestFinishedEvent $event): void
+            {
+                $this->extension->executeAfterTest(
+                    $event->test()->name(),
+                    (float) $event->telemetryInfo()->time()->seconds()
+                );
+            }
+        });
+
+        $facade->registerSubscriber(new class($extension) implements ErroredSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+
+            public function notify(Errored $event): void
+            {
+                $this->extension->executeAfterTestError(
+                    BootstrappedExtension::testName($event->test()),
+                    $event->throwable()->message(),
+                    (float) $event->telemetryInfo()->time()->seconds()
+                );
+            }
+        });
+
+        $facade->registerSubscriber(new class($extension) implements FailedSubscriber {
+            public function __construct(
+                private LegacyExtension $extension,
+            ) {
+            }
+
+            public function notify(Failed $event): void
+            {
+                $this->extension->executeAfterTestFailure(
+                    BootstrappedExtension::testName($event->test()),
+                    $event->throwable()->message(),
+                    (float) $event->telemetryInfo()->time()->seconds())
+                ;
+            }
+        });
+    }
+
+    public static function testName(Test $test) {
+        if ($test->isTestMethod()) {
+            return $test->nameWithClass();
+        }
+
+        return $test->name();
+    }
+
+    public static function registerBrowser(Browser $browser): void
+    {
+        LegacyExtension::registerBrowser($browser);
+    }
+}
---
+++ src/Browser/Test/LegacyExtension.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * This file is part of the zenstruck/browser package.
+ *
+ * (c) Kevin Bond <kevinbond@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zenstruck\Browser\Test;
+
+use Zenstruck\Browser;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+class LegacyExtension
+{
+    /** @var Browser[] */
+    private static array $registeredBrowsers = [];
+    private static bool $enabled = false;
+
+    /** @var array<string,array<string,string[]>> */
+    private array $savedArtifacts = [];
+
+    /**
+     * @internal
+     */
+    public static function registerBrowser(Browser $browser): void
+    {
+        if (!self::$enabled) {
+            return;
+        }
+
+        self::$registeredBrowsers[] = $browser;
+    }
+
+    public function executeBeforeFirstTest(): void
+    {
+        self::$enabled = true;
+    }
+
+    public function executeBeforeTest(string $test): void
+    {
+        self::reset();
+    }
+
+    public function executeAfterTest(string $test, float $time): void
+    {
+        foreach (self::$registeredBrowsers as $browser) {
+            foreach ($browser->savedArtifacts() as $category => $artifacts) {
+                if (!\count($artifacts)) {
+                    continue;
+                }
+
+                $this->savedArtifacts[$test][$category] = $artifacts;
+            }
+        }
+
+        self::reset();
+    }
+
+    public function executeAfterLastTest(): void
+    {
+        if (empty($this->savedArtifacts)) {
+            return;
+        }
+
+        echo "\n\nSaved Browser Artifacts:";
+
+        foreach ($this->savedArtifacts as $test => $categories) {
+            echo "\n\n  {$test}";
+
+            foreach ($categories as $category => $artifacts) {
+                echo "\n    {$category}:";
+
+                foreach ($artifacts as $artifact) {
+                    echo "\n      * {$artifact}:";
+                }
+            }
+        }
+    }
+
+    public function executeAfterTestError(string $test, string $message, float $time): void
+    {
+        self::saveBrowserStates($test, 'error');
+    }
+
+    public function executeAfterTestFailure(string $test, string $message, float $time): void
+    {
+        self::saveBrowserStates($test, 'failure');
+    }
+
+    private static function saveBrowserStates(string $test, string $type): void
+    {
+        if (empty(self::$registeredBrowsers)) {
+            return;
+        }
+
+        $filename = \sprintf('%s_%s', $type, self::normalizeTestName($test));
+
+        foreach (self::$registeredBrowsers as $i => $browser) {
+            try {
+                $browser->saveCurrentState("{$filename}__{$i}");
+            } catch (\Throwable $e) {
+                // noop - swallow exceptions related to dumping the current state so as to not
+                // lose the actual error/failure.
+            }
+        }
+    }
+
+    private static function normalizeTestName(string $name): string
+    {
+        // Try to match for a numeric data set index. If it didn't, match for a string one.
+        if (!\preg_match('#^([\w:\\\]+)(.+\#(\d+))#', $name, $matches)) {
+            \preg_match('#^([\w:\\\]+)(.+"([^"]+)")#', $name, $matches);
+        }
+
+        $normalized = \strtr($matches[1], '\\:', '-_');
+
+        if (isset($matches[3])) {
+            $normalized .= '__data-set-'.\strtr($matches[3], '\\: ', '-_-');
+        }
+
+        return $normalized;
+    }
+
+    private static function reset(): void
+    {
+        self::$registeredBrowsers = [];
+    }
+}
