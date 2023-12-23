e99afd50a23831bdfe9ffb8582304a23862d2094
Flohw <git@f-pa.site>
2023-12-23 09:59:10 +0100

    Update tests expected output

--- tests/NormalizationTest.php
+++ tests/NormalizationTest.php
@@ -77,27 +77,27 @@ final class NormalizationTest extends TestCase
         $baseTemplate = \strtr('error_'.__METHOD__."__data-set-", '\\:', '-_');
         yield 'self within moustache' => [
             'test name' => __METHOD__.' with data set "te{{self}}st" (test set)',
-            'expected output' => $baseTemplate.'te--self--st__0',
+            'expected output' => $baseTemplate.'te-self-st__0',
         ];
         yield 'double quoted with space' => [
             'test name' => __METHOD__.' with data set "_self.env.setCache("uri://host.net:2121") _self.env.loadTemplate("other-host")" (test set)',
-            'expected output' => $baseTemplate.'_self-env-setCache--uri---host-net-2121---_self-env-loadTemplate--other-host--__0',
+            'expected output' => $baseTemplate.'_self-env-setCache-uri-host-net-2121-_self-env-loadTemplate-other-host-__0',
         ];
         yield 'double quotes in moustache' => [
             'test name' => __METHOD__.' with data set "te{{_self.env.registerUndefinedFilterCallback("exec")}}{{_self.env.getFilter("id")}}st"',
-            'expected output' => $baseTemplate.'te--_self-env-registerUndefinedFilterCallback--exec------_self-env-getFilter--id----st__0',
+            'expected output' => $baseTemplate.'te-_self-env-registerUndefinedFilterCallback-exec-_self-env-getFilter-id-st__0',
         ];
         yield 'escaped simple quote' => [
             'test name' => __METHOD__.' with data set "te{{\'/etc/passwd\'|file_excerpt(1,30)}}st"',
-             'expected output' => $baseTemplate.'te----etc-passwd--file_excerpt-1-30---st__0',
+             'expected output' => $baseTemplate.'te-etc-passwd-file_excerpt-1-30-st__0',
          ];
         yield 'single quote for array index access' => [
             'test name' => __METHOD__.' with data set "te{{[\'id\']|filter(\'system\')}}st"',
-            'expected output' => $baseTemplate.'te----id---filter--system----st__0',
+            'expected output' => $baseTemplate.'te-id-filter-system-st__0',
         ];
         yield 'numeric array access' => [
             'test name' => __METHOD__.' with data set "te{{[0]|reduce(\'system\',\'id\')}}st"',
-            'expected output' => $baseTemplate.'te---0--reduce--system---id----st__0',
+            'expected output' => $baseTemplate.'te-0-reduce-system-id-st__0',
         ];
     }
 }
