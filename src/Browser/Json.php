d704215b5f2eb9cb23e270c79af5a1bfd0b31485
Kevin Bond <kevinbond@gmail.com>
2026-01-08 10:37:12 -0500

    mb_trim to trim

--- src/Browser/Json.php
+++ src/Browser/Json.php
@@ -167,7 +167,7 @@ final class Json
             return $this->decoded;
         }
 
-        if ('' === mb_trim($this->source)) {
+        if ('' === \trim($this->source)) {
             return null;
         }
 
